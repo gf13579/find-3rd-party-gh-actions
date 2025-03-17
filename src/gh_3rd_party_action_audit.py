b2e4ca8a44efa8e4896037a755e9cf44edcee6d4
gf13579 <greg.ford.2@googlemail.com>
2025-03-17 15:50:04 +1000

    Tidied up

--- src/gh_3rd_party_action_audit.py
+++ src/gh_3rd_party_action_audit.py
@@ -9,6 +9,7 @@ import base64
 
 GITHUB_API_URL = "https://api.github.com/search/code"
 
+
 def search_github(org):
     token = os.getenv("GITHUB_TOKEN")
     if not token:
@@ -20,18 +21,14 @@ def search_github(org):
 
     query = "uses: language:YAML path:.github/" + f" user:{org}"
 
-    regex_filter = fr"uses:\s*(?!\s*({org}|github|actions|\./\.github)/)"
+    regex_filter = rf"uses:\s*(?!\s*({org}|github|actions|\./\.github)/)"
 
     headers = {
         "Authorization": f"token {token}",
-        "Accept": "application/vnd.github.text-match+json"
+        "Accept": "application/vnd.github.text-match+json",
     }
 
-    params = {
-        "q": query,
-        "per_page": 100,
-        "page": 1
-    }
+    params = {"q": query, "per_page": 100, "page": 1}
 
     session = requests.Session()
     session.headers.update(headers)
@@ -51,13 +48,13 @@ def search_github(org):
             break
 
         for item in items:
-            repo = item['repository']['full_name']
-            path = item['path']
+            repo = item["repository"]["full_name"]
+            path = item["path"]
 
             # Get the file content as item.text_matches only contains the first two results
-            file_response = session.get(item['url'])
-            b64_content = file_response.json()['content']
-            file_content = base64.b64decode(b64_content).decode('utf-8')
+            file_response = session.get(item["url"])
+            b64_content = file_response.json()["content"]
+            file_content = base64.b64decode(b64_content).decode("utf-8")
 
             file_lines = file_content.splitlines()
             for line in file_lines:
@@ -65,112 +62,102 @@ def search_github(org):
                     new_match = {
                         "repo": repo,
                         "path": path,
-                        "line": line.replace("- ","").strip()
+                        "line": line.replace("- ", "").strip(),
                     }
                     if not line.startswith("#"):
                         all_matches_from_all_content.append(new_match)
 
-
-            # matches = item.get('text_matches', [])
-
-            # for match in matches:
-            #     fragment = match.get('fragment', '')
-
-            #     if re.search(regex_filter, fragment):
-            #         for line in fragment.split('\n'):
-            #             if re.search(regex_filter, line):
-            #                 new_match = {
-            #                     "repo": repo,
-            #                     "path": path,
-            #                     "line": line.replace("- ","").strip()
-            #                 }
-            #                 if not line.startswith("#"):
-            #                     all_matches.append(new_match)
-
-
         # Pagination
-        if 'next' in response.links:
-            params['page'] += 1
+        if "next" in response.links:
+            params["page"] += 1
         else:
             break
-    
-    # return all_matches
+
     return all_matches_from_all_content
 
+
 def write_markdown_summary(output_md, all_matches):
-    with open(output_md, 'w') as f:
+    with open(output_md, "w") as f:
 
         f.write("# Third-party GitHub Action Audit\n\n")
-
-        # Query run 2025-03-17 12:45:43. Results for default branches only.
-
-        f.write("Query run " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ". Results for default branches only.\n\n")
+        f.write(
+            "Query run "
+            + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            + ". Results for default branches only.\n\n"
+        )
 
         f.write("## Third party actions\n\n")
         f.write("|Count|Action|\n")
         f.write("|--:|:--|\n")
-        
+
         from collections import defaultdict
+
         counts = defaultdict(int)
         for match in all_matches:
-            counts[match['line']] += 1
-        
+            counts[match["line"]] += 1
+
         sorted_lines = sorted(counts.items(), key=lambda x: x[1], reverse=True)
-        
+
         for line, count in sorted_lines:
             action = line.replace("uses: ", "")
-            # Create a Confluence-compatible anchor link
+            # Confluence-compatible anchor link
             anchor = urllib.parse.quote_plus(action.lower())
 
             f.write(f"|{count}|[{action}](#{anchor})|\n")
-        
+
         f.write("\n---\n")
 
-       
         # Table of distinct_count(action) by repo
         f.write("## Number of distinct actions by repo\n\n")
         f.write("|Repository|Unique Actions|\n")
         f.write("|---|--:|\n")
-        
+
         repo_actions = defaultdict(set)
         for match in all_matches:
-            repo_name = match['repo'].split('/')[1]
-            action = match['line'].replace("uses: ", "")
+            repo_name = match["repo"].split("/")[1]
+            action = match["line"].replace("uses: ", "")
             repo_actions[repo_name].add(action)
-        
-        sorted_repos = sorted(repo_actions.items(), key=lambda x: len(x[1]), reverse=True)
+
+        sorted_repos = sorted(
+            repo_actions.items(), key=lambda x: len(x[1]), reverse=True
+        )
         for repo, actions in sorted_repos:
             f.write(f"|{repo}|{len(actions)}|\n")
 
         f.write("\n---\n")
 
         f.write("## References per action\n\n")
-        
-        unique_lines = set([match['line'] for match in all_matches])
+
+        unique_lines = set([match["line"] for match in all_matches])
         for line in unique_lines:
             third_party_action = line.replace("uses: ", "")
             f.write(f"\n### {third_party_action}\n\n")
             f.write("|Repository|Path|\n")
             f.write("|---|---|\n")
             for match in all_matches:
-                if match['line'] == line:
-                    repo_name = match['repo'].split('/')[1]
+                if match["line"] == line:
+                    repo_name = match["repo"].split("/")[1]
                     f.write(f"|{repo_name}|{match['path']}|\n")
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="GitHub Code Search with Advanced Filtering")
+    parser = argparse.ArgumentParser(
+        description="GitHub Code Search with Advanced Filtering"
+    )
 
     parser.add_argument("org", help="GitHub organization/user name")
-    parser.add_argument("--output-md", help="Output file for markdown summary", required=True)
+    parser.add_argument(
+        "--output-md", help="Output file for markdown summary", required=True
+    )
     parser.add_argument("--output-json", help="Optional output file for search results")
-    
+
     args = parser.parse_args()
 
     all_matches = search_github(args.org)
 
     if args.output_json:
         os.makedirs("results", exist_ok=True)
-        with open(f"results/{args.output_json}", 'w') as f:
+        with open(f"results/{args.output_json}", "w") as f:
             for match in all_matches:
                 f.write(f"{match}\n")
         print(f"Results written to results/{args.output_json}\n\n")
